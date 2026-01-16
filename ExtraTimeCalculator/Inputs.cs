b390f43d3d0b1b7fd39adb935489ba3c4c4a969d
Lu'ay <luaylamia@gmail.com>
2026-01-16 20:12:51 +0000

    Started calculating the endtime

--- ExtraTimeCalculator/Inputs.cs
+++ ExtraTimeCalculator/Inputs.cs
@@ -5,13 +5,13 @@ using System.Linq;
 
 public partial class Inputs : Node
 {
-	
+	//Nodes:
 	private LineEdit _DefualtDuration;
 	private LineEdit _ExtraTime;
 	private LineEdit _StartTime;
 
-	private string _Output = "00:00";
-	private int[] _InputTime = new int[2];
+	//Properties:
+	private string _startTime = "00:00";
 
 	[Signal]
 	public delegate void UpdateEndTimeEventHandler(string input);
@@ -27,20 +27,54 @@ public partial class Inputs : Node
 		_StartTime.TextSubmitted += OnStartTimeTextSubmitted;
 	}
 	
-	private void OnExtraTimeTextChanged(string newText)
-	{
-		EmitSignal(SignalName.UpdateEndTime, newText);
-	}
-
 	private void OnDefualtDurationTextChanged(string newText)
 	{
-		EmitSignal(SignalName.UpdateEndTime, newText);
+		
+
+		//EmitSignal(SignalName.UpdateEndTime, newText);
+	}
+	private void OnExtraTimeTextChanged(string newText)
+	{
+		//EmitSignal(SignalName.UpdateEndTime, newText);
 	}
 
 	private void OnStartTimeTextSubmitted(string newText)
-	{
-		EmitSignal(SignalName.UpdateEndTime, newText);
+	{		
+		if (newText.Contains(":"))
+		{
+			string[] input = newText.Split(":");
+			int hours = int.Parse(input[0]), minutes = int.Parse(input[1]);
+
+			if (minutes > -1 && minutes < 60 && hours > -1 && hours < 24)
+			{
+				//store
+			}
+		}
 		
+		//EmitSignal(SignalName.UpdateEndTime, newText);		
+	}
+
+	void CalculateEndTime(int[] startTime, int extraTime, int duration)
+	{
+		int totalDuration = (int)((float)duration*((float)duration/100));
+		int hours = 0, minutes = 0;
+
+		if(totalDuration > 59)
+		{
+			hours += totalDuration/60;
+			totalDuration -= hours*60;
+		}
+		if(minutes + totalDuration > 59)
+		{
+			hours += 1;
+			minutes += totalDuration - 60;
+		}
+		if (hours > 23)
+		{
+			hours = hours%24; //this allows for int.MaxValue hours!
+		}
+
+		//string output of hours and minutes.
 	}
 	
 }
--- ExtraTimeCalculator/Output.cs
+++ ExtraTimeCalculator/Output.cs
@@ -3,12 +3,6 @@ using System;
 
 public partial class Output : Label
 {
-	private Node _Inputs;
-	public override void _Ready()
-	{
-	   Node _Inputs = GetNode("%Inputs");
-	}
-
 	public void UpdateEndTimeEventHandler(string input)
 	{
 		this.Text = input;
