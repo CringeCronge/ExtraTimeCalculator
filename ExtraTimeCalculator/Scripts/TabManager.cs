b3f891fad1305b92c40991dd28b373c4b02b20b1
Lu'ay <luaylamia@gmail.com>
2026-01-21 08:07:55 +0000

    Added a simple tab menu

---
+++ ExtraTimeCalculator/Scripts/TabManager.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class TabManager : TabContainer
+{
+	private Button AddTab;
+	private Button DeleteTab;
+
+	public override void _Ready()
+	{
+		AddTab = GetNode<Button>("Set Up/GridContainer/AddTab");
+		DeleteTab = GetNode<Button>("Set Up/GridContainer/DeleteTab");
+
+		AddTab.Pressed += AddNewTab;
+		DeleteTab.Pressed += DeleteLastTab;
+	}
+
+	private void AddNewTab()
+	{
+		PackedScene newTab = ResourceLoader.Load<PackedScene>("res://Scenes/DefualtTab.tscn");
+		AddChild(newTab.Instantiate(), true, InternalMode.Disabled);
+	}
+
+	private void DeleteLastTab()
+	{
+		if(GetChildCount()>1)
+        {
+            var tabToDelete = GetChild(GetChildCount()-1);
+		    RemoveChild(tabToDelete);
+        }
+	}
+}
